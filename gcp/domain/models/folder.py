a93607f453db7ca88a076ef4f5feb6059d11b5eb
Bellanov Apilli <bellanovapilli@gmail.com>
2026-07-25 13:03:02 -0400

    Updated GCP folder name regex

--- gcp/domain/models/folder.py
+++ gcp/domain/models/folder.py
@@ -4,7 +4,7 @@ from pydantic import Field
 
 from gcp.domain.models.pydantic import CamelCaseModel
 
-GCP_FOLDER_NAME = r"^[a-z0-9\s\-_]+$"
+GCP_FOLDER_NAME = r"^[a-zA-Z0-9][a-zA-Z0-9 _-]{1,28}[a-zA-Z0-9]$"
 GCP_FOLDER_ID = r"^[a-z0-9-]+$"
 
 
