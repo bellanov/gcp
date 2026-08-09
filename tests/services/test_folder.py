7c51c6449bb94888a4b457cb1928662e5966733d
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-08 22:42:23 -0400

    Removed integration tests

--- tests/services/test_folder.py
+++ tests/services/test_folder.py
@@ -12,19 +12,13 @@ GCP_FOLDER_DISPLAY_NAME = os.getenv("GCP_FOLDER_DISPLAY_NAME")
 GCP_ORGANIZATION_ID = os.getenv("GCP_ORGANIZATION_ID")
 
 
-@pytest.mark.integration
-def test_get_folders():
-    """Test folder retrieval."""
-    assert get_folders_for_organization(organization_id=GCP_ORGANIZATION_ID) is not None
-
-
 @pytest.mark.unit
 def test_get_folder():
     """Test folder retrieval by name."""
     get_folder(id=GCP_FOLDER_ID, folder_name=GCP_FOLDER_NAME)
 
 
-@pytest.mark.unit
+@pytest.mark.integration
 def test_get_folders_for_organization():
     """Test get_folders_for_organization returns a generator."""
     for folder in get_folders_for_organization(GCP_ORGANIZATION_ID):
