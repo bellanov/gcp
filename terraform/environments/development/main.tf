6a050016ef2da37dcf886db6c7a23a2a65cba9e9
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-13 00:36:38 -0400

    Added developer service account

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -1,5 +1,5 @@
 
 resource "google_service_account" "service_account" {
-  account_id   = "github-actions-deploy-sa"
-  display_name = "Service Account to deploy via GitHub Actions"
+  account_id   = "developer-sa"
+  display_name = "Service Account for local development"
 }
