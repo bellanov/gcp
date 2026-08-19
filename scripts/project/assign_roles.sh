694e7edc85fbc5788507f305b365aa4cd7eef3f4
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-19 16:53:26 -0400

    Removed troubleshooting step

---
+++ scripts/project/assign_roles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Assign roles to a Deployment Service Account for GitHub Actions.
+
+gcloud config set project $GCP_PROJECT
+
+TIMESTAMP=$(date +%s)
+
+ROLES="roles/iam.serviceAccountTokenCreator
+roles/storage.admin"
+
+SERVICE_ACCOUNT=$1
+
+# Check if the service account has the required roles, if not grant them
+for role in $ROLES; do
+    if ! gcloud projects get-iam-policy $GCP_PROJECT \
+        --flatten="bindings[].members" \
+        --format="table(bindings.role)" \
+        --filter="bindings.members:${SERVICE_ACCOUNT}" | grep -q "$role"; then
+        gcloud projects add-iam-policy-binding $GCP_PROJECT \
+            --member="serviceAccount:${SERVICE_ACCOUNT}" \
+            --role="$role"
+        echo "Service account granted the role $role"
+    else
+        echo "Service account already has the role $role"
+    fi
+done
--- scripts/project/create_service_account.sh
+++ scripts/project/create_service_account.sh
@@ -1,38 +1,18 @@
 #!/bin/bash
 #
-# Create or update Deployment Service Account permissions.
+# Create a Deployment Service Account for GitHub Actions.
 
 gcloud config set project $GCP_PROJECT
 
 TIMESTAMP=$(date +%s)
 
-ROLES="roles/iam.serviceAccountAdmin
+ROLES="roles/iam.serviceAccountTokenCreator
 roles/storage.admin"
 
-PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT --format='value(projectNumber)')
-
-# Check if the service account exists, if not create it
-if ! gcloud iam service-accounts describe "github-actions-${TIMESTAMP}@${GCP_PROJECT}.iam.gserviceaccount.com" --project $GCP_PROJECT >/dev/null 2>&1; then
-    gcloud iam service-accounts create "github-actions-${TIMESTAMP}" \
+if gcloud iam service-accounts create "github-actions-${TIMESTAMP}" \
         --description="Deployment service account for GitHub Actions" \
-        --display-name="GitHub Actions Deployment Service Account"
-
+        --display-name="GitHub Actions Deployment Service Account"; then
     echo "Service account github-actions-${TIMESTAMP} created."
 else
-    echo "Service account github-actions-${TIMESTAMP} already exists."
+    echo "Failure creating Service Account { github-actions-${TIMESTAMP} } already exists."
 fi
-
-# Check if the service account has the required roles, if not grant them
-for role in $ROLES; do
-    if ! gcloud projects get-iam-policy $GCP_PROJECT \
-        --flatten="bindings[].members" \
-        --format="table(bindings.role)" \
-        --filter="bindings.members:github-actions-${TIMESTAMP}@${GCP_PROJECT}.iam.gserviceaccount.com" | grep -q "$role"; then
-        gcloud projects add-iam-policy-binding $GCP_PROJECT \
-            --member="serviceAccount:github-actions-${TIMESTAMP}@${GCP_PROJECT}.iam.gserviceaccount.com" \
-            --role="$role"
-        echo "Service account granted the role $role"
-    else
-        echo "Service account already has the role $role"
-    fi
-done
