796742d2ebaddaa15e09ed559d2d96f658cd193a
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-19 17:16:00 -0400

    Updated configuration

--- scripts/github/authenticate_pipeline.sh
+++ scripts/github/authenticate_pipeline.sh
@@ -7,11 +7,11 @@ gcloud config set project $GCP_PROJECT
 PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT --format=value\(projectNumber\))
 POOL_ID="github-actions-pool-1787166615"
 MEMBER="principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/attribute.repository/bellanov/google"
-SERVICE_ACCOUNT="github-actions-1787170169@${GCP_PROJECT}.iam.gserviceaccount.com"
+SERVICE_ACCOUNT="github-actions-178717379@${GCP_PROJECT}.iam.gserviceaccount.com"
 
 
 if gcloud storage buckets add-iam-policy-binding "gs://${GCP_PROJECT}" \
-    --role=roles/iam.serviceAccountTokenCreator \
+    --role=roles/storage.admin \
     --member="$MEMBER"; then
     echo "Bucket IAM policy binding applied."
 else
