48286440f0bcec7cb94f708c8529338c44e6d08e
Bellanov <bellanovapilli@gmail.com>
2026-08-21 18:28:49 -0400

    Update authenticate_pipeline.sh

--- terraform/scripts/authenticate_pipeline.sh
+++ terraform/scripts/authenticate_pipeline.sh
@@ -4,10 +4,14 @@
 
 gcloud config set project $GCP_PROJECT
 
-gcloud iam service-accounts add-iam-policy-binding "github-actions-1787174527@${GCP_PROJECT}.iam.gserviceaccount.com" \
+SERVICE_ACCOUNT="github-actions-1787174527@${GCP_PROJECT}.iam.gserviceaccount.com"
+
+# Grant workload identity user role to allow GitHub Actions to impersonate the service account
+gcloud iam service-accounts add-iam-policy-binding "$SERVICE_ACCOUNT" \
   --role="roles/iam.workloadIdentityUser" \
   --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
 
-gcloud storage buckets add-iam-policy-binding "gs://gcp-development-503118-1787174368" \
-  --role="roles/storage.admin" \
-  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
+# Grant storage permissions to the service account itself (project-level)
+gcloud projects add-iam-policy-binding "$GCP_PROJECT" \
+  --role="roles/storage.folderAdmin" \
+  --member="serviceAccount:$SERVICE_ACCOUNT"
