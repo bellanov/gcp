168d62f3313d2d1575af2d5631db9f485f1be21c
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-22 13:41:19 -0400

    Removed API enabling

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -3,17 +3,6 @@ provider "google" {
   project = var.project_id
 }
 
-resource "google_project_service" "service" {
-  for_each = toset(local.services)
-  project  = var.project_id
-  service  = each.value
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-}
-
 resource "google_service_account" "service_account" {
   account_id   = "developer"
   display_name = "Service Account for local development"
@@ -24,8 +13,6 @@ resource "google_service_account" "service_account" {
 }
 
 locals {
-  services = []
-
   # TODO: Dynamically generate service accounts and roles based on a map of service accounts and their roles
   service_accounts = {
     developer = {
