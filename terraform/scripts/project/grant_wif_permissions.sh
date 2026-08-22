7e6afb3f30aa1d27bd819692c652da7b8bc29046
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-22 13:38:41 -0400

    Refactored terraform scripts

--- terraform/scripts/project/grant_wif_permissions.sh
+++ terraform/scripts/project/grant_wif_permissions.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 #
 # Grant permissions to the Workload Identity User.
+# 
+# Prerequisites: The user running this script must have the following roles:
+# - Service Account Admin (to create/manage service accounts)
+# - Service Usage Admin (to enable APIs)
+# - Storage Admin (to manage buckets)
+# - IAM Security Admin (to bind IAM policies)
 
 gcloud config set project $GCP_PROJECT
 
 PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT --format=value\(projectNumber\))
 SERVICE_ACCOUNT="github-actions@${GCP_PROJECT}.iam.gserviceaccount.com"
-
-# Grant project-level permissions for API management
-gcloud projects add-iam-policy-binding $GCP_PROJECT \
-  --member="serviceAccount:${SERVICE_ACCOUNT}" \
-  --role="roles/serviceusage.serviceUsageAdmin"
-
-# Grant permissions to create and manage service accounts
-gcloud projects add-iam-policy-binding $GCP_PROJECT \
-  --member="serviceAccount:${SERVICE_ACCOUNT}" \
-  --role="roles/iam.serviceAccountAdmin"
 
 # Grant permissions to impersonate the service account
 gcloud iam service-accounts add-iam-policy-binding "$SERVICE_ACCOUNT" \
